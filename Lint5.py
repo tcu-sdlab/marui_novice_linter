21b27ac8e1fc0c029aadf203832d2b6483a3c1f4
maru713 <31789637+maru713@users.noreply.github.com>
2020-10-21 11:01:50 +0900

    two update

--- Lint5.py
+++ Lint5.py
@@ -1,22 +1,22 @@
 import ast
 import astor
 
-class FiveNodeVisitor(ast.NodeVisitor):
+class FiveSixNodeVisitor(ast.NodeVisitor):
 
     def visit_FunctionDef(self, node):
         pattern_num = 0
         node.body.append("")
         for i in range(len(node.body) - 1):
             if type(node.body[i]) is ast.If and type(node.body[i].body[0]) is ast.Return and 'value' in vars(node.body[i].body[0].value):
-                if node.body[i].body[0].value.value:                    
+                if type(node.body[i].body[0].value.value) is bool and node.body[i].body[0].value.value:                    
                     linenum = -1
 
                     if  type(node.body[i + 1]) is ast.Return and 'value' in vars(node.body[i + 1].value):
-                        if not node.body[i + 1].value.value:
+                        if type(node.body[i + 1].value.value) is bool and not node.body[i + 1].value.value:
                             linenum = node.body[i + 1].lineno
                     elif node.body[i].orelse:
                         if type(node.body[i].orelse[0]) is ast.Return and 'value' in vars(node.body[i].orelse[0].value):
-                            if not node.body[i].orelse[0].value.value:
+                            if type(node.body[i].orelse[0].value.value) is bool and not node.body[i].orelse[0].value.value:
                                 linenum = node.body[i].orelse[0].lineno
                             
                     if linenum != -1:
@@ -36,6 +36,53 @@ class FiveNodeVisitor(ast.NodeVisitor):
 
         del(node.body[-1])
         return node
+<<<<<<< Updated upstream
+=======
+class TwoNodeVisitor(ast.NodeVisitor):
+    except_list = []
+    elif_check = 0
+    def visit_If(self, node):
+        
+        if node not in self.except_list:
+            
+            conditions, lineno = self.search_childIf(node)
+            if conditions:
+                if len(conditions) > 1:
+                    print("line {0} ~ {1} :These conjoining conditions{2} using nested if statements can be simplified by using and operator.(pattern-2)".format(node.lineno, lineno, conditions))
+                    print("I suggest this code should be written")
+                    # print("elif_check={}".format(self.elif_check))
+                    if self.elif_check == 1:
+                        print("     elif ", end = "")
+                    else:
+                        print("     if ", end = "")
+                    i = 0
+                    for i in range(len(conditions) - 1):
+                        print(conditions[i],end = "")
+                        print(" and ",end = "")
+                    print(conditions[i + 1],end = "")
+                    print(":")
+                conditions.clear()
+        if node.orelse:
+            # print("行きます")
+            print(node.orelse)
+            self.elif_check = 1
+            self.visit(node.orelse[0])
+            # print("完了です")
+            self.elif_check = 0
+        self.generic_visit(node)
+        return node
+    def search_childIf(self, node, conditions_sub = []):
+        condition = astor.to_source(node.test).replace("\n","")
+        if type(node.test) is ast.Compare:
+            condition = condition.rstrip(")").lstrip("(")
+        conditions_sub.append(condition)
+        
+        if type(node.body[0]) is ast.If and not node.body[0].orelse and len(node.body) == 1:
+            self.except_list.append(node)
+            return self.search_childIf(node.body[0], conditions_sub)
+        else:
+            return conditions_sub, node.lineno
+>>>>>>> Stashed changes
 def AST_Reader(ast_code):
     
     count = 0
@@ -83,7 +130,13 @@ def main():
     tree = ast.parse(source, FILENAME)
     AST_Reader(tree)
 
+<<<<<<< Updated upstream
     FiveNodeVisitor().visit(tree)
+=======
+    #FiveSixNodeVisitor().visit(tree)
+    print()
+    TwoNodeVisitor().visit(tree)
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     main()
--- pydat.py
+++ pydat.py
@@ -12,40 +12,74 @@ This is test code for my linter
 #     return False
 
 def six(string):
-    """
-    6. The previous pattern for conditions with method calls instead of operators.
-    """
     if string.isdigit():
         return True
-    return False  # Unnecessary "else" after "return" (no-else-return)
+    return False  
 
 def five(pattern_five):
-    """
-    5.  Returning a boolean value by checking a condition
-        with an operator (e.g., ==, >, <) by using an if statement
-        and explicitly returning true or false rather than simply returning the condition.
-    """
     if pattern_five == 0:
         return True
-    return False  # Unnecessary "else" after "return" (no-else-return)
+    return False  
 
 def five_another(pattern_five_another):
-    """
-    5.  Returning a boolean value by checking a condition
-        with an operator (e.g., ==, >, <) by using an if statement
-        and explicitly returning true or false rather than simply returning the condition.
-    """
     if pattern_five_another == 0:
         return True
     else:
-        return False  # Unnecessary "else" after "return" (no-else-return)
+        return False 
 
 def five_dummy(pattern_five_dummy):
-    """
-    5.  Returning a boolean value by checking a condition
-        with an operator (e.g., ==, >, <) by using an if statement
-        and explicitly returning true or false rather than simply returning the condition.
-    """
     if pattern_five_dummy == 0:
         return True
+<<<<<<< Updated upstream
     return "False"  # Unnecessary "else" after "return" (no-else-return)
+=======
+    return "False" 
+
+def five_dummy2(pattern_five_dummy2):
+    if pattern_five_dummy2 == 0:
+        return 1
+    return 0  
+
+def two(val):
+    """
+    2. Conjoining conditions using nested if statements rather than the "and" operator.
+    """
+    if val < 10:
+        if val > 0:
+            return 0
+    return 1
+def two2(val):
+    """
+    2. Conjoining conditions using nested if statements rather than the "and" operator.
+    """
+    if val < 10:
+        if val > 0:
+            if val == 1:
+                return 0
+    return 1
+def two_dummy(val):
+    """
+    2. Conjoining conditions using nested if statements rather than the "and" operator.
+    """
+    if val < 10:
+        if val > 0:
+            if val == 1:
+                return 0
+        print(2)
+    return 1
+
+def two_dummy1(val):
+    """
+    2. Conjoining conditions using nested if statements rather than the "and" operator.
+    """
+    if val < 10:
+        print()
+        if val > 0:
+            if val == 1:
+                return 0
+    elif val < 15:
+        if val < 30:
+            return 2
+    return 1
+print(type(True) is bool)
+>>>>>>> Stashed changes
